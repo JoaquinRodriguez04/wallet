08457d06f38824f0af8f8562cdb887974162d5e2
Joaquin <JoaquindeveloperWeb@gmail.com>
2023-05-01 20:52:07 -0300

    se modifico el app.js

--- app.js
+++ app.js
@@ -1,24 +1,16 @@
 // variables
 
-// const modeDark = document.getElementById("mode_Dark")
-// const moon = document.getElementById("moon")
-// const body = document.querySelector("body")
-// const icons = document.querySelector("i")
-// const name_navbar = document.getElementById("name_navbar")
-// const wallet = document.getElementById("wallet")
-// const data_h2 = document.getElementById("data_h2")
-// const data_p = document.getElementById("data_p")
-// const data_white = document.getElementById("data")
-// const data_black = document.getElementById("data_2")
-// const data_black_2 = document.getElementById("data_3")
-// const btn_wallet = document.getElementById("btn_wallet")
-// const transfer = document.getElementById("transfer")
-// const request = document.getElementById("request")
-// const navbar_low = document.getElementById("navbar_low")
-// const dia_semana2 = document.getElementById("dia_semana")
-// const btn_sent = document.getElementById("btn_sentTo")
-// const beel = document.getElementById("beel")
-// const a_sentTo = document.getElementById("a_sentTo")
+// variables mode dark
+const modeDark = document.getElementById("mode_Dark")
+const body = document.querySelector("body")
+const select = document.getElementById("dia_semana")
+const name_user = document.getElementById("name_navbar")
+const wallet = document.getElementById("wallet")
+const transfer = document.getElementById("transfer")
+const request = document.getElementById("request")
+const i = document.getElementById("icon")
+const btn_sent = document.getElementById("btn_sentTo")
+const a_sentTo = document.getElementById("a_sentTo")
 
 // variables (menus desplegables)
 const close = document.getElementById("close")
@@ -30,16 +22,22 @@ const light_mode_etiquets = document.getElementById("light_mode_etiquets")
 const navbar_lightning = document.getElementById("navbar_lightning")
 const lightning = document.getElementById("lightning")
 
-// unciones
+// funciones
 
 // eventos
 
-// modeDark.addEventListener("click", () => {
-//     body.classList.toggle("dark_mode_tarjets");
-//     name_navbar.classList.toggle("light_mode_etiquets");
-//     data_black.classList.toggle("light_mode_etiquets");
-//     data_black_2.classList.toggle("light_mode_etiquets");
-// });
+modeDark.addEventListener("click", () => {
+    body.classList.toggle("black_body");
+    modeDark.classList.toggle("white");
+    select.classList.toggle("black");
+    name_user.classList.toggle("white");
+    wallet.classList.toggle("white_wallet");
+    transfer.classList.toggle("primary_transfer");
+    request.classList.toggle("primary_request");
+    i.classList.toggle("black");
+    btn_sent.classList.toggle("black_btn");
+    a_sentTo.classList.toggle("white_a");
+});
 
 perfil.addEventListener("click", () => {
     navbar_perfil.classList.add("nav_visible")
